d67ce40b1ecce2b742febc726c40855080ff3a3b
Nicolas-Verbaere <nverbaere@gmail.com>
2023-04-17 18:24:50 +0200

    convert html to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1 className="title">Pokedex Région de Kanto</h1>
+                <form>
+                    <div className="searchPkmn">
+                        <label for="name">Recherche par nom: </label>
+                        <input name="name" id="name" autocomplete="off" />
+                    </div>
+                </form>
+            </header>
+            <div className="cards">
+                <div className="card">
+                    <img
+                        className="imgPkmn"
+                        src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/4/regular.png"
+                        alt="avatar"
+                    />
+                    <div className="pkmnCard">
+                        <h4 className="namePkmn">Salameche</h4>
+                    </div>
+                </div>
+                <div className="card">
+                    <img
+                        className="imgPkmn"
+                        src="https://raw.githubusercontent.com/Yarkis01/PokeAPI/images/sprites/3/regular.png"
+                        alt="avatar"
+                    />
+                    <div className="pkmnCard">
+                        <h4 className="namePkmn">Florizarre</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
